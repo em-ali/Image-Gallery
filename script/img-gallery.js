afa5bd9a5bdc8e44b9b5eedfea523b6a89759028
emali <mal@codehousegroup.com>
2014-05-27 10:38:03 +0100

    Added additional todos and fixed syntax bugs.

--- script/img-gallery.js
+++ script/img-gallery.js
@@ -16,7 +16,11 @@
  * FEATURE: as jQuery plugin        DONE
  * FEATURE: Transition types        PARTIAL
  * FEATURE: lightbox integration    TODO
+ * FEATURE: Image preloading        TODO
+ * FEATURE: multimedia support      TODO
+ * FEATURE: Vertical thumbs slider  TODO
  * BUG:     Exception handling for user input for animation type, and slide direction   TODO
+ * BUG:     First load doesn't prohibit active slide from animating                     TODO
  *
  */
 
@@ -39,15 +43,12 @@
 
         return this.each(function(){ // return this for jquery chaining
 
-
-
             var config = $.extend({}, defaults, options); // options override defaults
 
             var $thumbnails,                                                // thumbnail items
                 $thumbnailImg,                                              // thumbnail item image
                 imgWidth,                                                   // image width
                 imgHeight,                                                  // image height
-                slideInDirection,                                           // slide in direction
                 src,                                                        // image src path
                 alt,                                                        // image alternative text
                 $imgGallery = $(this),                                      // gallery element
@@ -56,163 +57,157 @@
                 $topImg = $imgContainer.find('.top-img'),                   // top level image
                 $btmImg = $imgContainer.find('.btm-img'),                   // bottom level image
                 $activeImg,													// current active image
-                $nonActiveImg,											    // non active image
                 $targetImg,                                                 // target slide image for slide
-                $previousActiveImg                                          // previously active feature image
+                $previousActiveImg;                                          // previously active feature image
 
             var gallery = {
-                setup: function(){
-                    $thumbnails = $imgGallery.find('.thumb-item');
-
-                    if (gallery.lightboxTest()) {
-                        $('body').prepend( '<span class="overlay"></strong>' );
-                    }
-
-
-
-//                    if (!(slideDirection = "rightToLeft") || (slideDirection = "rightToLeft") || (slideDirection = "rightToLeft") || (slideDirection = "rightToLeft") ) { // check for correct options
-//
-//                    }
-
+                init: function () {
+                    gallery.setup();
                     gallery.imgUpdt($thumbnails.first());
                     gallery.events();
                 },
-                events: function(){
-                    $thumbnails.click(function (){
-                        if (!$imgContainer.children().is(':animated')){ // check if not animating
-                            if ($(this).hasClass('active')) { // check if selected thumb is already selected.
-                                return false;
-                            };
-                            gallery.imgUpdt($(this));
+                setup: function() {
+                    $thumbnails = $imgGallery.find('.thumb-item');
+
+                    if (gallery.factory.lightboxTest()) {
+                        $('body').prepend('<span class="overlay"></strong>');
+                    }
+                },
+                events: function() {
+                    $thumbnails.click(function() {
+                        if (!$imgContainer.children().is(':animated')) { // check if not animating
+                            if (!$(this).hasClass('active')) { // check if selected thumb is already selected.
+                                gallery.imgUpdt($(this));
+                            }
                         }
                     });
 
-                    $focusImg.click(function (){
+                    $focusImg.click(function() {
                         if ($('overlay:hidden')) { // check if modal window visible
                             gallery.lightbox($lightboxTrigger);
                         }
-                    })
+                    });
                 },
                 imgUpdt: function($thumbnailTrigger) { // select thumnail item image
                     $thumbnailImg = $thumbnailTrigger.children('img'); // clicked thumbnail image element
-
-                    gallery.atrUpdt($thumbnailTrigger);
+                    gallery.attrUpdt($thumbnailTrigger);
                 },
                 fade: function($thumbnailTrigger) {
                     if ($topImg.hasClass('active')) { // if top img has active class
                         gallery.btmImgUpdt($thumbnailTrigger); // update bottom image with attributes
-                        $topImg.fadeOut(config.animationSpeed, function () { // fade top opacity to 0
-                            gallery.setActive($thumbnailTrigger) // set active classes
+                        $topImg.fadeOut(config.animationSpeed, function() { // fade top opacity to 0
+                            gallery.setActive($thumbnailTrigger); // set active classes
                         });
                     } else {
                         gallery.topImgUpdt(); // update top img with attributes
-                        $topImg.fadeIn(config.animationSpeed, function (){ // fadein top opacity to 100
-                            gallery.setActive($thumbnailTrigger) // set active classes
+                        $topImg.fadeIn(config.animationSpeed, function() { // fadein top opacity to 100
+                            gallery.setActive($thumbnailTrigger); // set active classes
                         });
                     }
                 },
-                slide: function($thumbnailTrigger){
-                	$activeImg = $imgContainer.find('img.active');
+                slide: function($thumbnailTrigger) {
+                    $activeImg = $imgContainer.find('img.active');
                     gallery.setTargetImg();
 
                     var cssProps, animProp;
-                    switch(config.slideDirection){
-                        case "leftToRight":
-                            cssProps = { 'left' : imgWidth , 'z-index' : '1'};
-                            animProp = {'left' : '-=' + imgWidth  };
-                            break;
-                        case "rightToLeft":
-                            cssProps = { 'right' : imgWidth , 'z-index' : '1'};
-                            animProp = {'right' : '-=' + imgWidth  };
-                            break;
-                        case "bottomToTop":
-                            cssProps = { 'top' : imgHeight, 'z-index' : '1' }
-                            animProp = { 'top' : '-=' + imgHeight }
-                            break;
-                        case "topToBottom":
-                            cssProps = { 'top' : '-=' + imgHeight, 'z-index' : '1' }
-                            animProp = { 'top' : imgHeight - imgHeight }
-                            break;
+                    switch (config.slideDirection) {
+                    case "leftToRight":
+                        cssProps = { 'left': imgWidth, 'z-index': '1' };
+                        animProp = { 'left': '-=' + imgWidth };
+                        break;
+                    case "rightToLeft":
+                        cssProps = { 'right': imgWidth, 'z-index': '1' };
+                        animProp = { 'right': '-=' + imgWidth };
+                        break;
+                    case "bottomToTop":
+                        cssProps = { 'top': imgHeight, 'z-index': '1' };
+                        animProp = { 'top': '-=' + imgHeight };
+                        break;
+                    case "topToBottom":
+                        cssProps = { 'top': '-=' + imgHeight, 'z-index': '1' };
+                        animProp = { 'top': imgHeight - imgHeight };
+                        break;
                     }
-                    
-                    gallery.makeSlide(cssProps, animProp, $thumbnailTrigger);  
+
+                    gallery.makeSlide(cssProps, animProp, $thumbnailTrigger);
                 },
-                makeSlide: function (cssProp, animProp, $thumbnailTrigger){
-                        $targetImg.css(cssProp);
+                makeSlide: function(cssProp, animProp, $thumbnailTrigger) {
+                    $targetImg.css(cssProp);
 
-                        gallery.setActiveAttr();
+                    gallery.setActiveAttr();
 
-                        $previousActiveImg.css('z-index', 0);
+                    $previousActiveImg.css('z-index', 0);
 
-                        $targetImg.animate(animProp, config.animationSpeed, config.animationType, function (){
-                            gallery.setActive($thumbnailTrigger); // set active classes
-                        });
+                    $targetImg.animate(animProp, config.animationSpeed, config.animationType, function() {
+                        gallery.setActive($thumbnailTrigger); // set active classes
+                    });
                 },
                 slideActions: {
-                    common: function (cssProp, animProp, $thumbnailTrigger){
+                    common: function(cssProp, animProp, $thumbnailTrigger) {
                         $targetImg.css(cssProp);
 
                         gallery.setActiveAttr();
 
                         $previousActiveImg.css('z-index', 0);
 
-                        $targetImg.animate(animProp, config.animationSpeed, config.animationType, function (){
-                            gallery.setActive($thumbnailTrigger) // set active classes
+                        $targetImg.animate(animProp, config.animationSpeed, config.animationType, function() {
+                            gallery.setActive($thumbnailTrigger); // set active classes
                         });
                     }
                 },
                 topImgUpdt: function() { // top image attribute update
                     $topImg.attr({
                         'src': src,
-                        'alt': alt,
+                        'alt': alt
                     });
                 },
                 btmImgUpdt: function() { // bottom image attribute update
                     $btmImg.attr({
                         'src': src,
-                        'alt': alt,
+                        'alt': alt
                     });
                 },
-                setActiveAttr: function () {
-                    if ($topImg.hasClass('active')){
+                setActiveAttr: function() {
+                    if ($topImg.hasClass('active')) {
                         gallery.btmImgUpdt(); // update bottom image with attributes
                     } else {
                         gallery.topImgUpdt(); // update top image with attributes
                     }
                 },
                 setTargetImg: function() {
-                    if ($topImg.hasClass('active')){
+                    if ($topImg.hasClass('active')) {
                         $targetImg = $btmImg;
                         $previousActiveImg = $topImg;
                     } else {
                         $targetImg = $topImg;
                         $previousActiveImg = $btmImg;
                     }
-                    console.log($targetImg);
+                    //console.log($targetImg);
                 },
-                atrUpdt: function($thumbnailTrigger) { // get image attributes
-                    src = $thumbnailImg.attr('src'),
-                    alt = $thumbnailImg.attr('alt'),
+                attrUpdt: function($thumbnailTrigger) { // get image attributes
+                    src = $thumbnailImg.attr('src');
+                    alt = $thumbnailImg.attr('alt');
                     imgWidth = $topImg.width();
                     imgHeight = $topImg.height();
 
-                    switch (gallery.transitionType()) { // test for what transistion type is set
+                    // test for what transistion type is set
+                    switch (gallery.factory.transitionType()) {
                         case 'fade':
-                            (gallery.ActiveImgTest()) ? gallery.fade($thumbnailTrigger) : gallery.dfltAtrUpdt($thumbnailTrigger);
-                        break;
+                            (gallery.factory.ActiveImgTest()) ? gallery.fade($thumbnailTrigger) : gallery.dfltattrUpdt($thumbnailTrigger);
+                            break;
                         case 'slide':
-                            (gallery.ActiveImgTest()) ? gallery.slide($thumbnailTrigger) : gallery.dfltAtrUpdt($thumbnailTrigger);
-                        break;
+                            (gallery.factory.ActiveImgTest()) ? gallery.slide($thumbnailTrigger) : gallery.dfltattrUpdt($thumbnailTrigger);
+                            break;
                         default:
-                            gallery.dfltAtrUpdt($thumbnailTrigger);
+                            gallery.dfltattrUpdt($thumbnailTrigger);
                     }
                 },
-                dfltAtrUpdt: function ($thumbnailTrigger) { // default case for image update
+                dfltattrUpdt: function($thumbnailTrigger) { // default case for image update
                     gallery.topImgUpdt(src, alt);
                     gallery.setActive($thumbnailImg, $thumbnailTrigger);
 
                 },
-                setActive: function ($thumbnailTrigger) {
+                setActive: function($thumbnailTrigger) {
                     $thumbnails.removeClass('active');
                     $thumbnailTrigger.addClass('active');
 
@@ -231,28 +226,27 @@
                         }
                     }
                 },
-                lightbox: function($lightboxTrigger){
-                    gallery.atrUpdt($lightboxTrigger);
-                    $(".overlay").prepend('<img src="'+ src +'" alt="'+ alt +'"/>');
+                lightbox: function($lightboxTrigger) {
+                    gallery.attrUpdt($lightboxTrigger);
+                    $(".overlay").prepend('<img src="' + src + '" alt="' + alt + '"/>');
                 },
-                slideDirectionTest: function(){
-	                // remove test delete if redunent
-                },
-                // testing methods
-                transitionType: function() { // transition type test
-                    return (config.fade) ? "fade" : (config.slide) ? "slide" : "default";
-                },
-                ActiveImgTest: function () { // check
-                    return ($imgContainer.children().hasClass('active')) ? true : false ;
-                },
-                lightboxTest: function () { // check
-                    return (config.zoomLightbox) ? true : false ;
-                },
-                errorHandling: function() {
+                // factory methods
+                factory: {
+                    transitionType: function() { // transition type test
+                        return (config.fade) ? "fade" : (config.slide) ? "slide" : "default";
+                    },
+                    ActiveImgTest: function() { // check
+                        return ($imgContainer.children().hasClass('active'));
+                    },
+                    lightboxTest: function() { // check
+                        return (config.zoomLightbox);
+                    },
+                    errorHandling: function() {
 
+                    }
                 }
-            }
-            gallery.setup();
+            };
+            gallery.init();
         });
     };
 }(jQuery));
